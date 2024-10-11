54e19d858d3a223df10394d8d596dcac2b3fa9dd
Maxim Martyshov <m.martyshov@innopolis.university>
2024-10-11 14:08:27 +0300

    feat: model registry implemented

---
+++ src/get_model.py
@@ -0,0 +1,4 @@
+import mlflow.pytorch
+
+def load_best_model(model_name):
+    model_uri = f''
--- src/test.py
+++ src/test.py
@@ -1,9 +1,2 @@
-from zenml.client import Client
-
-# Get the ZenML client
-client = Client()
-
-# Get the local store path
-local_store_path = client.zen_store
-
-print(f"ZenML Local Store Path: {local_store_path}")
+import sys 
+sys.path.append("/Users/maxmartyshov/Desktop/IU/year3/PMDL/Sentiment_Analysis_for_Financial_News/notebooks")
