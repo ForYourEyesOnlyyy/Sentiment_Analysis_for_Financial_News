8ad5fb63ce94bd82fdeb54efe1bd6ef5d385f95d
Maxim Martyshov <m.martyshov@innopolis.university>
2024-10-15 16:51:23 +0300

    refactor: ruff

--- pipelines/training_data_pipeline.py
+++ pipelines/training_data_pipeline.py
@@ -1,15 +1,13 @@
+import sys
+
 sys.path.append(
     "/Users/maxmartyshov/Desktop/IU/year3/PMDL/Sentiment_Analysis_for_Financial_News/src"
 )
 
 import data
-
 from zenml.steps import step
 from zenml.pipelines import pipeline
-
 import pandas as pd
-
-import sys
 
 
 @step
