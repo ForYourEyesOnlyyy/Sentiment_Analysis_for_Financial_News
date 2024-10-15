e681d83ee9133df0278c80d304dd7079a9d7578c
Maxim Martyshov <m.martyshov@innopolis.university>
2024-10-15 16:27:00 +0300

    feat: inference pipeline up and running; slow though

---
+++ pipelines/inference_pipeline.py
@@ -0,0 +1,95 @@
+from zenml.steps import step, BaseParameters
+from zenml.pipelines import pipeline
+
+from transformers import AutoTokenizer
+
+import pandas as pd
+
+import sys
+
+sys.path.append(
+    "/Users/maxmartyshov/Desktop/IU/year3/PMDL/Sentiment_Analysis_for_Financial_News/src"
+)
+
+import data
+import inference
+
+class InferencePipelineParams(BaseParameters):
+    tweet: str
+    tokenizer_name:str = 'bert-base-uncased'
+    max_length: int = 100
+    model_name: str = 'simple_sentiment_analysis_model'
+    device: str = 'cpu'
+
+@step
+def make_dataframe(params: InferencePipelineParams) -> pd.DataFrame:
+    return data.make_dataframe_with_dummy_label(params.tweet)
+
+@step 
+def preprocess(df: pd.DataFrame) -> pd.DataFrame:
+    return data.preprocess_data(df)
+
+@step
+def make_dataset(params: InferencePipelineParams, df: pd.DataFrame) -> data.FinancialTweetsDataset:
+    return data.FinancialTweetsDataset(df[data.text_column], df[data.has_source_column], df[data.label_column], data.get_tokenizer(params.tokenizer_name), params.max_length)
+
+@step
+def predict(params: InferencePipelineParams, dataset: data.FinancialTweetsDataset) -> str:
+    prediction = inference.predict(params.model_name, dataset, params.device)
+    return prediction
+
+
+@pipeline
+def inference_pipeline(make_dataframe, preprocess, make_dataset, predict):
+    tweet = make_dataframe()
+    preprocessed = preprocess(tweet)
+    dataset = make_dataset(preprocessed)
+    predictions = predict(dataset)
+    return predictions
+
+if __name__ == "__main__":
+    pipeline_params = InferencePipelineParams(
+        tweet="$ANCUF: BMO Capital Markets ups to Outperform",
+        tokenizer= data.get_tokenizer('bert-base-uncased'),
+        max_length=100,
+        model_name='simple_sentiment_analysis_model',
+        device='mps'
+    )
+
+    make_dataframe_instance = make_dataframe(params=pipeline_params)
+    preprocess_instance = preprocess()
+    make_dataset_instance = make_dataset(params=pipeline_params)
+    predict_instance = predict(params=pipeline_params)
+
+    inference_pipeline_instance = inference_pipeline(
+        make_dataframe=make_dataframe_instance,
+        preprocess=preprocess_instance,
+        make_dataset=make_dataset_instance,
+        predict=predict_instance
+    )
+    from time import time
+    start = time()
+    inference_pipeline_instance.run()
+    # Access the prediction result from ZenML
+    from zenml.client import Client
+    inf_start = time()
+    client = Client()
+    pipeline = client.get_pipeline("inference_pipeline")
+    latest_run = pipeline.last_run
+    predict_step_result = latest_run.steps["predict"].output.load()
+
+    # Printing the prediction
+    print("Prediction:", predict_step_result)
+    print("Time taken to run the pipeline + loading:", time()-start)
+    print("Time taken to load the prediction:", time()-inf_start)
+
+'''
+    Time taken to run the pipeline + loading: 1.7046940326690674
+    Time taken to load the prediction: 0.05875706672668457
+    -------------------------------------------------------
+    Time taken to run the pipeline + loading: 3.325381278991699
+    Time taken to load the prediction: 0.07668924331665039
+'''
+
+
+
--- src/inference.py
+++ src/inference.py
@@ -3,7 +3,7 @@ from mlflow.tracking import MlflowClient
 
 import torch
 
-from transformers import AutoTokenizer
+from time import time
 
 import data
 
@@ -24,7 +24,9 @@ def load_model_from_registry(model_name, version="champion"):
     print(f"Model {model_name} loaded successfully")
     return model
 
-def predict(model_name, dataset: data.FinancialTweetsDataset, device) -> str:
+def predict(model_name:str, dataset: data.FinancialTweetsDataset, device:str) -> str:
+    dataset = dataset[0]
+
     model = load_model_from_registry(model_name)
     model.to(device)
     model.eval()
@@ -42,14 +44,31 @@ def predict(model_name, dataset: data.FinancialTweetsDataset, device) -> str:
         else:
             return data.sentiments.get(int(preds[0]), 'Unknown')
         
-# test_tweet = "Copa Holdings stock price target raised to $130 from $103 at Deutsche Bank"
-# dataframe = data.make_dataframe_with_dummy_label(test_tweet)
-# dataframe = data.preprocess_data(dataframe)
-# tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
-# dataset = data.FinancialTweetsDataset(dataframe[data.text_column], dataframe[data.has_source_column], dataframe[data.label_column], tokenizer)
-# dataset = dataset[0]
-# pred = predict('simple_sentiment_analysis_model', dataset, 'mps')
-# print(pred)
-        
 
+# EXAMPLE USAGE
+'''
+    test_tweet = "Copa Holdings stock price target raised to $130 from $103 at Deutsche Bank"
+    dataframe = data.make_dataframe_with_dummy_label(test_tweet)
+    dataframe = data.preprocess_data(dataframe)
+    dataset = data.FinancialTweetsDataset(dataframe[data.text_column], dataframe[data.has_source_column], dataframe[data.label_column])
+    dataset = dataset[0]
+    pred = predict('simple_sentiment_analysis_model', dataset, 'mps')
+    print(pred)
+'''
+        
+# def run_inference(tweet: str, tokenizer: any, max_length: int, model_name: str, device: str)-> str:
+#     tweet_df = data.make_dataframe_with_dummy_label(tweet)
+#     tweet_df = data.preprocess_data(tweet_df)
+#     tweet_dataset = data.FinancialTweetsDataset(tweet_df[data.text_column], tweet_df[data.has_source_column], tweet_df[data.label_column], tokenizer, max_length)
+#     return predict(model_name, tweet_dataset, device)
+
+# if __name__ == "__main__":
+#     start = time()
+#     test_tweet = "$ANCUF: BMO Capital Markets ups to Outperform"
+#     tokenizer = data.get_tokenizer('bert-base-uncased')
+#     max_length = 100
+#     model_name = 'simple_sentiment_analysis_model'
+#     device = 'cpu'
+#     print(run_inference(test_tweet, tokenizer, max_length, model_name, device))
+#     print('Inference time:', time() - start)
 
