5d6ddef882a306f65b70312f646c9535788097ee
Maxim Martyshov <m.martyshov@innopolis.university>
2024-10-28 16:00:53 +0300

    feat: add sphinx docs folder

---
+++ docs/source/conf.py
@@ -0,0 +1,27 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Sentiment Analysis for Financial News'
+copyright = '2024, Maxim Martyshov, Elisey Smirnov, Roman Makeev'
+author = 'Maxim Martyshov, Elisey Smirnov, Roman Makeev'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
